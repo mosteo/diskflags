d2f5d25bc1eb1aeee239a16f3f977e6d1cd0f443
Alejandro R. Mosteo <amosteo@unizar.es>
2023-09-02 14:35:30 +0200

    Minimal feats

---
+++ src/diskflags.adb
@@ -0,0 +1,88 @@
+with Ada.Directories;
+
+with GNAT.OS_Lib;
+
+package body Diskflags is
+
+   package Dirs renames Ada.Directories;
+
+   -----------------
+   -- Create_Tree --
+   -----------------
+
+   procedure Create_Tree (Path : String) is
+   begin
+      if Dirs.Exists (Path) then
+         if Dirs.Kind (Path) in Dirs.Directory then
+            return;
+         else
+            raise Constraint_Error with
+              "Path already exists and is not a folder: " & Path;
+         end if;
+      else
+         Create_Tree (Dirs.Containing_Directory (Path));
+         Dirs.Create_Directory (Path);
+      end if;
+   end Create_Tree;
+
+   -----------
+   -- Touch --
+   -----------
+
+   procedure Touch (File : String) is
+      use GNAT.OS_Lib;
+      Success : Boolean := False;
+   begin
+      if Is_Regular_File (File) then
+         Set_File_Last_Modify_Time_Stamp (File, Current_Time);
+      elsif Dirs.Exists (File) then
+         raise Constraint_Error with
+           "Can't touch non-regular file: " & File;
+      else
+         Close (Create_File (File, Binary), Success);
+         if not Success then
+            raise Constraint_Error with
+              "Could not touch new file: " & File;
+         end if;
+      end if;
+   end Touch;
+
+   --------------
+   -- New_Flag --
+   --------------
+
+   function New_Flag (Path : Some_Path) return Flag
+   is
+      Full : constant Some_Path := Some_Path (Dirs.Full_Name (String (Path)));
+   begin
+      return (Length => Full'Length,
+              Path   => Full);
+   end New_Flag;
+
+   ------------
+   -- Exists --
+   ------------
+
+   function Exists (This : Flag) return Boolean
+   is (Dirs.Exists (String (This.Path))
+       and then Dirs.Kind (String (This.Path)) in Dirs.Ordinary_File);
+
+   ----------
+   -- Mark --
+   ----------
+
+   procedure Mark (This : in out Flag;
+                   Done : Boolean)
+   is
+   begin
+      if Done then
+         Create_Tree (Dirs.Containing_Directory (String (This.Path)));
+         Touch (String (This.Path));
+      else
+         if This.Exists then
+            Dirs.Delete_File (String (This.Path));
+         end if;
+      end if;
+   end Mark;
+
+end Diskflags;
--- src/diskflags.ads
+++ src/diskflags.ads
@@ -1,3 +1,36 @@
 package Diskflags is
 
+   type Flag (<>) is tagged private;
+
+   type Some_Path is new String;
+
+   function New_Flag (Path : Some_Path) return Flag;
+   --  This is the destination file whose operation we want to ensure was
+   --  completed, including the file flag: a download/copy destination, for
+   --  example. E.g.: /path/to/myflag. During creation, it will converted to
+   --  absolute if it isn't already.
+
+   function Exists (This : Flag) return Boolean;
+   --  Say if the flag exists (so whatever it signals has happened)
+
+   procedure Mark (This : in out Flag;
+                   Done : Boolean)
+     with Post => This.Exists = Done;
+   --  Set/remove the canary flag of the operation on path being complete. This
+   --  should be called when the operation has actually been completed.
+
+   function Path (This : Flag) return Some_Path;
+
+private
+
+   type Flag (Length : Natural) is tagged record
+      Path : Some_Path (1 .. Length);
+   end record;
+
+   ----------
+   -- Path --
+   ----------
+
+   function Path (This : Flag) return Some_Path is (This.Path);
+
 end Diskflags;
